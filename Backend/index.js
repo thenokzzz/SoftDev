9dd00c9d2b9511a5355198f11e77b5b8235cf882
Rockyyy <muhammad.535230104@stu.untar.ac.id>
2025-06-03 09:10:32 +0700

    Revert "update responsive"
    This reverts commit 10f1a9d71bb0b0e549961133135a57b623a4f6a0.

--- Backend/index.js
+++ Backend/index.js
@@ -40,6 +40,9 @@ app.use(cors());
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
+app.get("/", (req, res) => {
+  res.render("form");
+});
 
 app.post("/pay", midtrans);
 app.use(express.static(path.join(__dirname, "../Frontend")));
@@ -51,9 +54,7 @@ app.use("/api", campaignRoute);
 app.use("/api", galeriRoute);
 app.use("/api", donationRoute);
 
-app.get("/", (req, res) => {
-  res.send("Server aktif.");
-});
+
 app.get("/home", (req, res) => {
   res.sendFile(path.join(__dirname, "../Frontend/HTML/home.html"));
 });
